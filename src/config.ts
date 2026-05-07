5f864d65d77c0b308374c535bc10acae4fc315c1
Abdul Rahman Ameen <164730155+abdulrahmanqdev@users.noreply.github.com>
2026-05-07 22:43:33 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,3 @@ export const config = {
     token: "bot_token",
     clientId: "bot_id",
   };
-
-  //slow3rxq tarafından typescript kodlamak isteyenler için yapılan proje daha fazla proje için https://discord.gg/DRCE9wCn4K sunucumuza gelebilirsiniz.
-  //slow3rxq tarafından typescript kodlamak isteyenler için yapılan proje daha fazla proje için https://discord.gg/DRCE9wCn4K sunucumuza gelebilirsiniz.
-  //slow3rxq tarafından typescript kodlamak isteyenler için yapılan proje daha fazla proje için https://discord.gg/DRCE9wCn4K sunucumuza gelebilirsiniz.
-  //slow3rxq tarafından typescript kodlamak isteyenler için yapılan proje daha fazla proje için https://discord.gg/DRCE9wCn4K sunucumuza gelebilirsiniz.
